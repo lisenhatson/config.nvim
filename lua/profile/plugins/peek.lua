6fd176b349219ee4fcd63e5b4fa6b2893a5baf0d
Lisen Hatson <alimanthereploid@gmail.com>
2025-04-15 15:49:35 +0700

    update README

--- lua/profile/plugins/peek.lua
+++ lua/profile/plugins/peek.lua
@@ -5,6 +5,7 @@ return {
         build = "deno task --quiet build:fast",
         config = function()
             require("peek").setup({
+                app = { 'thorium-browser', '--new-window' },
                 filetype = { 'markdown', 'conf' }
             })
             vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
