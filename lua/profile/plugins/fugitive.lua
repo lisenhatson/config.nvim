436b7ec7d64a93d535b59851c33301c1f0dab60e
Lisen Hatson <alimanthereploid@gmail.com>
2025-04-23 07:54:07 +0700

    as per my last email

--- lua/profile/plugins/fugitive.lua
+++ lua/profile/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gj", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
         local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("lisenhatson_Fugitive", {})
 
@@ -16,7 +16,7 @@ return {
 
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>gs", function()
+                vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
                 end, opts)
 
