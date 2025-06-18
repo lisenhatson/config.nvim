71c404531f28eaefc44efbb8f5435675b176b915
Lisen Hatson <alimanthereploid@gmail.com>
2025-06-18 11:31:40 +0700

    colorscheme config

--- lua/profile/plugins/colorschemes.lua
+++ lua/profile/plugins/colorschemes.lua
@@ -1,19 +1,21 @@
 -- This is Primeagen's ColorMyPencils + TJ's colorschemes
 --
 function SetColor(color)
-	color = color or "everblush"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
+
     -- Transparency
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- The main background in normal text
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- On non-currently selected windows
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-    -- vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" }) -- On the line number
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" }) -- Left side of the line number
+    vim.api.nvim_set_hl(0, "ModeMsg", { bg = "none" }) -- the --INSERT-- etc
+    vim.api.nvim_set_hl(0, "MsgArea", { bg = "none" }) -- commandline area ":"
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" }) -- end of bufer (~~~~)
     vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "cyan" })
+    vim.api.nvim_set_hl(0, "Comment", { fg = "white" })
 end
 
 return {
